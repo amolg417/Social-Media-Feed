589fb963b655761f8125548132adb3f0e94931d4
amolg417 <amolg417@gmail.com>
2025-01-11 23:27:21 +0530

    Implemented review captured image feature

--- src/components/CreatePost/CameraSection.tsx
+++ src/components/CreatePost/CameraSection.tsx
@@ -1,55 +1,80 @@
 import { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import leftArrow from "../../assets/Profile/svg/SmLeft.svg";
-import flipCamera from "../../assets/Post/svg/flip-camera.svg"
-type CameraSectionProps={
-  toggleCamera:()=>void
-}
-const CameraSection = ({toggleCamera}:CameraSectionProps) => {
+import flipCamera from "../../assets/Post/svg/flip-camera.svg";
+import ReviewImage from "./ReviewImage";
+type CameraSectionProps = {
+  toggleCamera: () => void;
+};
+const CameraSection = ({ toggleCamera }: CameraSectionProps) => {
   const webcamRef = useRef<Webcam>(null);
   const [isFrontCamera, setIsFrontCamera] = useState(true);
-  const changeCamera=useCallback(()=>{
-    setIsFrontCamera(prev=>!prev)
-  },[])
+  const [capturedImage, setCapturedImage] = useState<null | string>(null);
+  const [reviewImage, setReviewImage] = useState(false);
+
+  const handleReviewImage = useCallback((newValue: boolean) => {
+    if(newValue===false){
+      setCapturedImage(null)
+    }
+    setReviewImage(newValue);
+  }, []);
+
+  const changeCamera = useCallback(() => {
+    setIsFrontCamera((prev) => !prev);
+  }, []);
+
   const capture = useCallback(() => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
-      console.log(imageSrc);
+      setCapturedImage(imageSrc);
     }
   }, [webcamRef]);
   return (
-    <div className="w-full h-full flex flex-col items-center absolute top-0 left-0 bg-[#fff] z-50">
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={"100%"}
-        height={"100%"}
-        className="w-full h-full object-cover"
-        videoConstraints={{
-          facingMode: isFrontCamera ? "user" : "environment",
-        }}
-        style={{ width: "100%", height: "100%" }}
-      />
-      <button
-        onClick={capture}
-        className="w-16 aspect-square rounded-full flex items-center justify-center bg-[#fff] fixed bottom-8 z-40"
-      >
-        <div className="w-14 aspect-square rounded-full bg-[#fff] border-[0.5rem] border-[#bbb6b6]"></div>
-      </button>
-      <img
-        src={leftArrow}
-        alt="closeIcon"
-        className="w-8 absolute top-2 left-2"
-        onClick={toggleCamera}
-      />
-      <img
-        src={flipCamera}
-        alt="flipCamera"
-        className="w-10 absolute bottom-11 right-14"
-        onClick={changeCamera}
-      />
-    </div>
+    <>
+      {reviewImage ? <ReviewImage imageSrc={capturedImage} baackToCamera={handleReviewImage}/> : <></>}
+      <div className="w-full h-full flex flex-col items-center absolute top-0 left-0 bg-[#fff] z-50">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width={"100%"}
+          height={"100%"}
+          className="w-full h-full object-cover"
+          videoConstraints={{
+            facingMode: isFrontCamera ? "user" : "environment",
+          }}
+          style={{ width: "100%", height: "100%" }}
+        />
+        <button
+          onClick={capture}
+          className="w-16 aspect-square rounded-full flex items-center justify-center bg-[#fff] fixed bottom-8 z-40"
+        >
+          <div className="w-14 aspect-square rounded-full bg-[#fff] border-[0.5rem] border-[#bbb6b6]"></div>
+        </button>
+        <img
+          src={leftArrow}
+          alt="closeIcon"
+          className="w-8 absolute top-2 left-2"
+          onClick={toggleCamera}
+        />
+        <img
+          src={flipCamera}
+          alt="flipCamera"
+          className="w-10 absolute bottom-11 right-14"
+          onClick={changeCamera}
+        />
+        {capturedImage ? (
+          <img
+            src={capturedImage}
+            alt="flipCamera"
+            className="w-10 aspect-square rounded absolute bottom-11 left-14"
+            onClick={() => handleReviewImage(true)}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/components/CreatePost/ReviewImage.tsx
+++ src/components/CreatePost/ReviewImage.tsx
@@ -1,17 +1,23 @@
 import leftArrow from "../../assets/Profile/svg/SmLeft.svg";
-const ReviewImage = () => {
+type reviewImageProps = {
+  imageSrc: null | string;
+  baackToCamera: (newValue: boolean) => void;
+};
+const ReviewImage = ({ imageSrc, baackToCamera }: reviewImageProps) => {
   return (
     <div className="w-full h-full flex justify-center absolute top-0 left-0 z-[70] bg-[#fff]">
-      <img src="https://s3-alpha-sig.figma.com/img/b54f/d858/f5e14f76f0793df709ce9bfe5e5f284e?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OUayc-gYnwYG2c5jBWaNT4FCWqxLcpIzlYZrRbOdbL-ALD~DIjPePMFn8qMGdvxE1KtrjNqSgTqcpScQiae~W21LVeN31Uwk~kkOvyVkyArnlc3f~QEpmFK2r8xrHLAnn2jgY4E3Q8tzlE~89tTOYuiSmWSCJOXAdHnvkjPngRQw0Y0Bh4~1W1TZbUwRCN5G7U5CHbm4~bwAkIbOWeHqKsGXfSc6foIhyv2RXRywlHBQwxOl4TDFKkCjeWpsySvRRIFS8GxTcDKDHZ6fvlDo0pbbzEfeUO5nL3tqMqk6x96OUnqIa5RgGHve48JlYMsFYWECzAMQibEj57UKQeFaQw__" alt="image" className="w-full h-full object-cover" />
+      <img src={imageSrc??""} alt="image" className="w-full h-full object-cover" />
       <img
         src={leftArrow}
         alt="closeIcon"
         className="w-8 absolute top-2 left-2"
-        // onClick={}
+        onClick={() => baackToCamera(false)}
       />
-      <button className="fixed bottom-4 w-[90%] py-3 text-center text-[#fff] bg-black font-[500] rounded-lg">PROCEED</button>
+      <button className="fixed bottom-4 w-[90%] py-3 text-center text-[#fff] bg-black font-[500] rounded-lg">
+        PROCEED
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ReviewImage
+export default ReviewImage;
